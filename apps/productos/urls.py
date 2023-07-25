5fb4bd08bfa4b4b76a0fe79b2a63834199274eb8
Mariano Quiroga <losquirogamir@gmail.com>
2023-07-24 21:06:43 -0300

    💥 NEW: Nuevas urls y html actualizados

--- apps/productos/urls.py
+++ apps/productos/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 
 from . import views
@@ -5,6 +7,22 @@ from . import views
 app_name = "productos"
 
 urlpatterns = [
+    # CATEGORIAS
+    path("crear/categorias/", views.crear_categorias, name="crear_categorias"),
+    path("crear/editar_categorias/<int:pk>/", views.UpdateCategorias.as_view(), name="editar_categoria"),
+    path("productos/eliminar_categoria/<int:pk>/", views.eliminar_categoria, name="eliminar_categoria"),
+    # SUB CATEGORIAS
+    path("crear/sub_categorias/", views.crear_sub_categorias, name="crear_subcategorias"),
+    path("productos/eliminar_subcategoria/<int:pk>/", views.eliminar_subcategoria, name="eliminar_subcategoria"),
+    path("crear/editar_subcategorias/<int:pk>/", views.UpdateSubCategorias.as_view(), name="editar_subcategoria"),
+    # AMBAS
+    path("sub_categorias/", views.sub_categorias, name="mostrar_categorias_subcategorias"),
+    # Esto asegura que se carguen correctamente los archivos de static
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
     path("crear/", views.CrearProductos.as_view(), name="crear_productos"),
-    path("productos/", views.ListProductos.as_view(), name="listar_productos"),
+    path("productos/", views.ListProductos.as_view(), name="productos"),
+    path("productos/eliminar_producto/<int:pk>/", views.eliminar_producto, name="eliminar_producto"),
+    path("productos/editar_producto/<int:pk>/", views.UpdateProductos.as_view(), name="editar_producto"),
 ]
--- apps/productos/views.py
+++ apps/productos/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import DeleteView, DetailView, ListView
 from django.views.generic.edit import CreateView, UpdateView
-from productos.models import Productos
+from productos.models import Categorias, Productos, SubCategorias
 
-# carpetas de apps
+from apps import productos
+
+from .forms import CategoriasForm, ProductosForm, SubCategoriasForm
 
 
 class CrearProductos(CreateView):
     model = Productos
     template_name = "productos/crear.html"
-    success_url = reverse_lazy("productos")
-    fields = "__all__"
+    form_class = ProductosForm
+    success_url = reverse_lazy("productos:productos")
+
+
+class UpdateProductos(UpdateView):
+    model = Productos
+    template_name = "productos/crear.html"
+    form_class = ProductosForm
+    success_url = reverse_lazy("productos:productos")
+
+    def get_pk(self, pk):
+        producto = Productos.objects.get(id=pk)
+        return producto
 
 
 class ListProductos(ListView):
@@ -19,8 +33,121 @@ class ListProductos(ListView):
     template_name = "productos/productos.html"
     fields = "__all__"
 
-    def listar_productos(self, **kwargs):
-        contexto = super().get_context_data(**kwargs)
-        productos = Productos.objects.all()
-        contexto["productos"] = productos
-        return contexto
+    def get_queryset(self):
+        query = self.request.GET.get("buscar")
+        if query:
+            productos = Productos.objects.filter(nombre__icontains=query)
+            if not productos:
+                messages.warning(self.request, "No se encontraron resultados")
+            return productos
+        else:
+            productos = Productos.objects.all()
+        return productos
+
+
+def eliminar_producto(request, pk):
+    producto = Productos.objects.get(id=pk)
+    producto.delete()
+    messages.success(request, "Se ha eliminado el producto exitosamente.")
+    return redirect("productos:productos")
+
+
+# Ordenar por nombre
+# * Ver después
+# def ordenar_productos(self, request):
+#     sort_by = self.request.POST.get("sort_criteria")
+
+#     if sort_by == "nombre":
+#         orden = self.get_queryset().order_by("nombre")
+#         print("Se ha elegido ordenar por nombre")
+#     elif sort_by == "precio":
+#         orden = self.get_queryset().order_by("nombre")
+#         print("Se ha elegido ordenar por precio")
+#     elif sort_by == "categoria":
+#         orden = self.get_queryset().order_by("nombre")
+#         print("Se ha elegido ordenar por categoría")
+#     else:
+#         orden = self.get_queryset().order_by("nombre")
+#         print("Se ha elegido ordenar por subcategoría")
+
+#     return (request, {"data": orden}, "productos/productos.html")
+
+
+# * Muestra todas las subcategorías y categorías en un solo template
+def sub_categorias(request):
+    contexto = {}
+    categorias = Categorias.objects.all()
+    sub_categorias = SubCategorias.objects.all()
+    contexto["categorias"] = categorias
+    contexto["sub_categorias"] = sub_categorias
+
+    return render(request, "productos/sub_categorias.html", contexto)
+
+
+def crear_categorias(request):
+    form = CategoriasForm()
+
+    if request.method == "POST":
+        form = CategoriasForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Ver mensaje
+            categoria = form.cleaned_data.get("categoria_name")
+            messages.success(request, f"Categoria creada exitosamente: {categoria}")
+            return redirect("index")
+
+    contexto = {"form": form}
+    return render(request, "productos/crear.html", contexto)
+
+
+class UpdateCategorias(UpdateView):
+    model = Categorias
+    template_name = "productos/crear.html"
+    form_class = CategoriasForm
+    success_url = reverse_lazy("index")
+
+    def get_pk(self, pk):
+        categoria = Categorias.objects.get(id=pk)
+        return categoria
+
+
+def eliminar_categoria(request, pk):
+    categoria = Categorias.objects.get(id=pk)
+    categoria.delete()
+    messages.success(request, "Se ha eliminado la categoria exitosamente.")
+    return redirect("index")
+
+
+# SUB CATEGORIAS
+class UpdateSubCategorias(UpdateView):
+    model = SubCategorias
+    template_name = "productos/crear.html"
+    form_class = SubCategoriasForm
+    success_url = reverse_lazy("index")
+
+    def get_pk(self, pk):
+        subcategoria = SubCategorias.objects.get(id=pk)
+        return subcategoria
+
+
+def crear_sub_categorias(request):
+    form = SubCategoriasForm()
+
+    if request.method == "POST":
+        form = SubCategoriasForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Ver mensaje
+            subcategoria = form.cleaned_data.get("sub_categoria")
+            messages.success(request, f"Subcategoria creada exitosamente:{subcategoria}")
+            return redirect("index")
+
+    contexto = {"form": form}
+    return render(request, "productos/crear.html", contexto)
+
+
+def eliminar_subcategoria(request, pk):
+    sub_categoria = SubCategorias.objects.get(id=pk)
+    sub_categoria.delete()
+    messages.success(request, "Se ha eliminado la subcategoria exitosamente.")
+    return redirect("index")
