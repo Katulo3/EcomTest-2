5b04260174d98ffd76808dc712a59fb9099b2c01
Mariano Quiroga <losquirogamir@gmail.com>
2023-07-24 21:07:17 -0300

    💥 NEW: Tienda basics

--- apps/Tienda/views.py
+++ apps/Tienda/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from productos.models import Productos
 
-# Create your views here.
+
+def tienda(request):
+    productos = Productos.objects.all()
+    contexto = {"productos": productos}
+    return render(request, "Tienda/tienda.html", contexto)
